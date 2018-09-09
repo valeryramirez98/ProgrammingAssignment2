8af1b375fe8971ff814fd3a2dfce04bf43c101a8
valeryramirez98 <42686601+valeryramirez98@users.noreply.github.com>
2018-09-08 23:44:45 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,19 @@
+#This function store and cache a inverse matrix
+
 makeCacheMatrix <- function(x =matrix()) {
        
  s <- NULL
        
  set <- function(y) {
                
- x <<- y
+ x <<- y #set the matrix in the cache
                
  s <<- NULL
       
   }
         
-get <- function() x       
-setsolve <- function(solve) s<<- solve 
+get <- function() x       #get the matrx
+setsolve <- function(solve) s<<- solve  #set matrix
 getsolve<- function() s     
   list(set = set, get = get,
            
@@ -20,7 +22,8 @@ getsolve<- function() s
   getsolve = getsolve)
 }
 
-
+#Here the function will calculate the inverse
+#Check if the inverse has already been calculated, in case if it throws the saved value otherwise it will calculate the inverse
 cacheSolve<- function(x, ...) {
         
 s <- x$getsolve()
